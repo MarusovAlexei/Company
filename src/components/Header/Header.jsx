6e416c3927daf8122e6a3de7f830f069c14c7f8f
Alexei <marusovalexei@gmail.com>
2023-03-18 12:43:58 +0300

    add: add header styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { ReactDOM, React } from "react";
+
 import "./Header.scss";
 import logoImg from "./../image/header/logo.png";
 import blackThemeImg from "./../image/header/blackThemeImg.png";
@@ -14,32 +15,34 @@ function Header() {
           </div>
           <div className="header__logo-name">name compony</div>
         </div>
-        <div className="header__menu">
-          <div className="header__menu-nav">
-            <div className="text-menu">Main</div>
-            <div className="text-menu">Map</div>
-            <div className="text-menu">Contact</div>
-            <div className="text-menu">About</div>
-            <div className="text-menu">Partners</div>
+        <div className="header__nav">
+          <div className="header__menu">
+            <div className="header__menu-nav">
+              <div className="text-menu">Main</div>
+              <div className="text-menu">Map</div>
+              <div className="text-menu">Contact</div>
+              <div className="text-menu">About</div>
+              <div className="text-menu">Partners</div>
+            </div>
+            <div className="header__menu-search">
+              <input
+                className="menu-search"
+                type="text"
+                placeholder={"enter the name of the residential complex"}
+              />
+            </div>
           </div>
-          <div className="header__menu-search">
-            <input
-              className="menu-search"
-              type="text"
-              placeholder={"enter the name of the residential complex"}
-            />
+          <div className="header__language">
+            <div className="header__language-text">Language</div>
+            <div className="header__language-btn">
+              <button className="language__btn-dropdown">english</button>
+              <div className="dropdown-content text-menu">russian</div>
+              <div className="dropdown-content text-menu">english</div>
+            </div>
           </div>
-        </div>
-        <div className="header__language">
-          <div className="header__language-text">Language</div>
-          <div className="header__language-btn">
-            <button className="language__btn-dropdown">english</button>
-            <div className="dropdown-content text-menu">russian</div>
-            <div className="dropdown-content text-menu">english</div>
+          <div className="header__theme-btn">
+            <img src={blackThemeImg} alt="theme" />
           </div>
-        </div>
-        <div className="header__theme-btn">
-          <img src={blackThemeImg} alt="theme" />
         </div>
       </div>
     </>
