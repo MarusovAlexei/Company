1d0b6c77e380d1af5d6c02c96a10ecc7b02919b6
Alexei <marusovalexei@gmail.com>
2023-03-21 15:50:45 +0300

    add: add dropDown menu

--- src/components/Header/DropDownMenu/DropDownMenu.jsx
+++ src/components/Header/DropDownMenu/DropDownMenu.jsx
@@ -1,14 +1,34 @@
 import "./DropDownMenu.scss";
+import React, { useState } from "react";
+
+const DropdownContent = (props) => {
+  return <div className="dropdown-content">{props.content}</div>;
+};
 
 const DropwnMenu = () => {
+  const [dropdown, setDropdown] = useState(false);
+
   return (
     <>
       <div className="header__language">
         <div className="header__language-text">Language</div>
         <div className="header__language-btn">
-          <button className="language__btn-dropdown">english</button>
-          <div className="dropdown-content text-menu">russian</div>
-          <div className="dropdown-content text-menu">english</div>
+          <button
+            className="language__btn-dropdown"
+            onClick={() => setDropdown(!dropdown)}
+          >
+            english
+          </button>
+          <div
+            className={
+              dropdown
+                ? "isVisible dropdown-content__wrapper"
+                : "inVisible dropdown-content__wrapper"
+            }
+          >
+            <DropdownContent content="russian" />
+            <DropdownContent content="english" />
+          </div>
         </div>
       </div>
     </>
--- src/components/Header/NavMenu/NavMenu.jsx
+++ src/components/Header/NavMenu/NavMenu.jsx
@@ -1,18 +1,18 @@
 import "./NavMenu.scss";
 
 const MenuBtn = (props) => {
-  return <div className="text-menu">{props.name}</div>;
+  return <div className="text-menu">{props.content}</div>;
 };
 
 const NavMenu = () => {
   return (
     <>
       <div className="header__menu-nav">
-        <MenuBtn name="Main" />
-        <MenuBtn name="Map" />
-        <MenuBtn name="Contact" />
-        <MenuBtn name="About" />
-        <MenuBtn name="Partners" />
+        <MenuBtn content="Main" />
+        <MenuBtn content="Map" />
+        <MenuBtn content="Contact" />
+        <MenuBtn content="About" />
+        <MenuBtn content="Partners" />
       </div>
     </>
   );
