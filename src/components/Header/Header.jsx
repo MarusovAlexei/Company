649fbb21387024b4f390e8706064e6fd5febb4d3
Alexei <marusovalexei@gmail.com>
2023-04-05 12:24:00 +0300

    add: add new variables

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,14 +10,16 @@ function Header() {
   return (
     <>
       <header className="header">
-        <Logo />
-        <div className="header__nav">
-          <div className="header__menu">
-            <NavMenu />
-            <SearchPannel />
+        <div className="header__container">
+          <Logo />
+          <div className="header__nav">
+            <div className="header__menu">
+              <NavMenu />
+              <SearchPannel />
+            </div>
+            <DropwnMenu />
+            <ToggleTheme />
           </div>
-          <DropwnMenu />
-          <ToggleTheme />
         </div>
       </header>
     </>
