1ad230e7cb549d8db0109c8dc0b782ca1e6d860f
Alexei <marusovalexei@gmail.com>
2023-06-01 22:55:47 +0300

    add: add country page

---
+++ src/components/HeaderSection.jsx
@@ -0,0 +1,5 @@
+const HeaderSection = (prop) => {
+  return <h2 className="header-section">{prop}</h2>;
+};
+
+export default HeaderSection;
--- src/pages/Country/Country.jsx
+++ src/pages/Country/Country.jsx
@@ -1,10 +1,41 @@
 import "./Country.scss";
+import { Link, useLocation } from "react-router-dom";
+import Belarus from "./../../assets/image/map/Belarus.png";
+import Russia from "./../../assets/image/map/Russia.png";
+import Azerbaijan from "./../../assets/image/map/Azerbaijan.png";
+import Cyprus from "./../../assets/image/map/Cyprus.png";
+import Georgia from "./../../assets/image/map/Georgia.png";
+import Turkey from "./../../assets/image/map/Turkey.png";
+import UAE from "./../../assets/image/map/UAE.png";
 
 const Country = () => {
+  const country = useLocation().pathname.substring(1);
+  const countyes = [
+    { img: Belarus, country: "Belarus" },
+    { img: Russia, country: "Russia" },
+    { img: Azerbaijan, country: "Azerbaijan" },
+    { img: Cyprus, country: "Cyprus" },
+    { img: Georgia, country: "Georgia" },
+    { img: Turkey, country: "Turkey" },
+    { img: UAE, country: "UAE" },
+  ];
+
+  const targetCountry = countyes.map((elem) => {
+    if (elem.country === country) {
+      return elem.img;
+    }
+  });
+
   return (
-    <>
-      <div>Country</div>
-    </>
+    <div className="country">
+      <div className="country__container">
+        <h2 className="country__header">{country}</h2>
+        <img src={targetCountry.join("")} alt="country" />
+        <Link className="country__btn" to={"/"}>
+          back to map
+        </Link>
+      </div>
+    </div>
   );
 };
 
